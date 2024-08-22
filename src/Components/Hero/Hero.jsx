40300926fad2e13b2b64ad7b01cb826fa979d53e
DamikaSulaksen <damikadesilva@gmail.com>
2024-08-22 19:08:46 +0530

    add section links

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
       <img src={profile_img} alt=""/>
       <h1><span>I'm Damika De Silva,</span> frontend developer based in USA.</h1>
       <p>A paragraph is a group of sentences that fleshes out a single idea. In order for a paragraph to be effective, it must begin with a topic sentence, have sentences that support the main idea of that paragraph, and maintain a consistent flow.</p>
